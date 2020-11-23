2397c468ae0b8a1895f50598508cbb8677160ea4
HomeAssMW <74811387+HomeAssMW@users.noreply.github.com>
2020-11-23 22:38:52 +0530

    Update auto.js

--- auto.js
+++ auto.js
@@ -1,4 +1,28 @@
 var p=0;
+
+function initi()
+{ 
+    for(p=1;p<11;p++)
+    {
+    document.getElementById("txtHint2").innerHTML=p;
+    if (document.getElementById("butt"+p).checked != false)
+    {
+        document.getElementById("butt"+p+"status2").style.display = "block";
+        document.getElementById("butt"+p+"status1").style.display = "none";
+        document.getElementById("txtHint3").innerHTML="turning on";
+        
+
+    } 
+    else if (document.getElementById("butt"+p).checked == false)
+    {
+        document.getElementById("butt"+p+"status1").style.display = "block";
+        document.getElementById("butt"+p+"status2").style.display = "none";
+        document.getElementById("txtHint3").innerHTML="turning off";
+
+    }
+    }
+}
+
 function req(p)
 {
     document.getElementById("txtHint2").innerHTML=p;
