a587424b4b391b6155ffe7bbe9e948230dbd50cb
HomeAssMW <74811387+HomeAssMW@users.noreply.github.com>
2021-03-30 13:23:49 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {
+  apiKey: "AIzaSyAlCeK_xNfjgQMPqNdFUB8Gc0yFzIj2Nys",
+  authDomain: "homeass-2528.firebaseapp.com",
+  databaseURL: "https://homeass-2528.firebaseio.com",
+  projectId: "homeass-2528",
+  storageBucket: "homeass-2528.appspot.com",
+  messagingSenderId: "973082369587",
+  appId: "1:973082369587:web:54c67a1630b2a044594d46",
+  measurementId: "G-24BQMQSFLL",
+}
